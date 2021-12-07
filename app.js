eea86bdd2acad7a5d84320d8750596f249ad6a18
bvisvanathan <balaji.visvanathan@gmail.com>
2021-12-07 19:39:32 +0530

    second commit 20211207_2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("This is schema valdiator");
+const mongoose = require("mongoose");
+const connectDatabase = require("./Database/DbConnect");
+const Schema = mongoose.Schema;
+const url = "mongodb://localhost:27017/shoppingtestapp";
+
+mongoose.pluralize(null);
+
+connectDatabase(url);
+
+const loadTestData = async () => {
+  console.log("Load Test");
+};
+
+loadTestData();
+
+//console.log("This is schema valdiator");
